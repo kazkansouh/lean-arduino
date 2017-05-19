6e73f10748992b7bb5f519552a992f95e6b7e884
kazza <kaz.kanso@gmail.com>
2017-05-19 13:23:42 +0100

    updated spi library to support multibyte transmissions

--- lib/spi.c
+++ lib/spi.c
@@ -8,9 +8,45 @@ void spi_master_init(void) {
   SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
 }
 
-void spi_master_transmit(char cData) {
+void spi_master_transmit(uint8_t i_data) {
   /* Start transmission */
-  SPDR = cData;
+  SPDR = i_data;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+    ;
+}
+
+void spi_master_transmit_16(uint16_t i_data) {
+  /* Transmit high byte */
+  SPDR = i_data >> 8;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+    ;
+  /* Transmit low byte */
+  SPDR = i_data & 0xFF;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+    ;
+}
+
+void spi_master_transmit_32(uint32_t i_data) {
+  /* Transmit high byte */
+  SPDR = i_data >> 24;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+    ;
+  /* Transmit next byte */
+  SPDR = (i_data >> 16) & 0xFF;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+    ;
+  /* Transmit next byte */
+  SPDR = (i_data >> 8) & 0xFF;
+  /* Wait for transmission complete */
+  while(!(SPSR & (1<<SPIF)))
+    ;
+  /* Transmit low byte */
+  SPDR = i_data & 0xFF;
   /* Wait for transmission complete */
   while(!(SPSR & (1<<SPIF)))
     ;
--- lib/spi.h
+++ lib/spi.h
@@ -1,7 +1,11 @@
 #ifndef _SPI_H
 #define _SPI_H
 
+#include <stdint.h>
+
 void spi_master_init(void);
-void spi_master_transmit(char cData);
+void spi_master_transmit(uint8_t i_data);
+void spi_master_transmit_16(uint16_t i_data);
+void spi_master_transmit_32(uint32_t i_data);
 
 #endif
