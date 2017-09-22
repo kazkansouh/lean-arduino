632ed2a819bdc36513c8ef894eb3a8a7f7bfe726
kazza <kaz.kanso@gmail.com>
2017-09-22 19:03:10 +0100

    load raw binary files directly into PROGMEM

--- dac/main.c
+++ dac/main.c
@@ -2,25 +2,26 @@
 #include <util/delay.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <avr/pgmspace.h>
 
 #define PAUSE_MS 1
 #define DELAY_MS 500
 
-extern unsigned char _binary_hello_8u_full_raw_start;
-extern unsigned char _binary_hello_8u_full_raw_end;
-extern unsigned char _binary_hello_8u_full_raw_size;
+extern const PROGMEM unsigned char _binary_hello_8u_full_raw_start;
+extern const PROGMEM unsigned char _binary_hello_8u_full_raw_end;
+extern const PROGMEM unsigned char _binary_hello_8u_full_raw_size;
 
 int main (void) {
-  uint8_t *table = &_binary_hello_8u_full_raw_start;
+  const uint8_t *table = &_binary_hello_8u_full_raw_start;
   DDRD = 0xFF; /* port D to output */
 
   while (true) {
-    PORTD = *table;
+    PORTD = pgm_read_byte(table);
     if (++table >= &_binary_hello_8u_full_raw_end) {
       table = &_binary_hello_8u_full_raw_start;
       _delay_ms(DELAY_MS);
     }
-    _delay_us(512);
+    _delay_us(530);
   }
   return 0;
 }
