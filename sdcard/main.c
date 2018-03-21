443335bee1e671296419d421d8fa927e97066a91
kazza <kaz.kanso@gmail.com>
2018-03-21 20:24:30 +0000

    first iteration that is capable of reading 8 bit unsigned pcm from sdcard and writing it to portd for dac, see layout.png. * program was developed against non-buffered 44.1KHz 8bit audio.

--- sdcard/main.c
+++ sdcard/main.c
@@ -1,9 +1,12 @@
+#include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-#include "usart_p.h"
 #include "usart.h"
 #include "sdcard.h"
 #include "sdcard-fat.h"
+
+#include "pins.h"
 
 #define MYUBRR F_CPU/16/BAUD-1
 
@@ -18,6 +21,17 @@
   pin4 |-> pin12 (MISO)
   pin3 |-> pin11 (MOSI)
   pin2 |-> pin10 (output/SS)
+  pin1 |-> pin9  (error pin, defined in makefile)
+
+  PORTD
+  pin0 |-> pin0 (LSB)
+  pin1 |-> pin1
+  pin2 |-> pin2
+  pin3 |-> pin3
+  pin4 |-> pin4
+  pin5 |-> pin5
+  pin6 |-> pin6
+  pin7 |-> pin7 (MSB)
 
   Connected to: SD Card
   Arduino pin13 (SCK) connected to (SCK)
@@ -25,78 +39,88 @@
   Arduino pin11 (MOSI) connected to (DI)
   Arduino pin10 (SS) connected to (CS)
 
-  Opens a file on sdcard and prints its bytes to the terminal.
+  Connected to: R2R Ladder
+  All pins of PORTD are connected to the R2R
+  Ladder
+
+  Description:
+  Program demonstrates playing audio files that are stored on a SD
+  card and writing the output to PORTD that is connected to a R2R
+  ladder for DAC. The input file is expected to be unsigned 8bit
+  uncompressed PCM with no headers (i.e. biased at 128). In tests, the
+  system was capable of playing data sampled at 44.1KHz. To create a
+  valid file, Audacity was used to export a clip of the audio. I.e:
+    File -> Export -> Select "Other uncompressed files"
+    Select "Options...", then
+      Headers: RAW
+      Encoding: Unsigned 8 bit PCM
 */
 
-/* change this to the name of the file, uses unix syntax and supports
-   vfat names */
-#define FILENAME "/test.txt"
-
-/* declare space for file system structures */
 SSDCard g_sdcard;
 SSDFATCard g_sdfatcard;
 
 int main(void) {
   uint8_t r;
 
-  /* enable interrupts, used for usart */
-  sei();
+  DDRD = 0xFF; /* port D to output */
 
-  /* initilise the usart and write boot message */
-  usart_init(MYUBRR);
-  usart_printf_P(PSTR("SDCard Demonstrator\n"));
+  /* flash led on on boot */
+  writePin(PIN_ERROR, true);
+  setMode(PIN_ERROR, output);
+  _delay_ms(250);
+  writePin(PIN_ERROR, false);
+
+  /* enable interrupts, used for timer */
+  sei();
 
   /* initilise the sdcard interface (includes spi) */
   r = sdcard_init(&g_sdcard);
   if (r != 0) {
+    usart_init(MYUBRR);
+    usart_printf("Could not init sdcard");
     goto end;
   }
 
   /* initilise the fat partition structure */
   r = fat32_init(&g_sdcard, &g_sdfatcard);
   if (r != 0) {
+    usart_init(MYUBRR);
+    usart_printf("Could not init fat");
     goto end;
   }
 
-  /* read the file */
   {
     SSDFAT_File s_sdfile;
     int16_t byte;
-    uint32_t size = 0;
 
     r = fat32_file_open(&g_sdfatcard,
                         &s_sdfile,
-                        FILENAME);
+                        FILE_NAME);
     if (r != 0) {
-      goto end;
-    }
-
-    /* read the file byte by byte  */
-    while ((byte = fat32_file_read_byte(&s_sdfile)) >= 0) {
-      if (size % 16 == 0) {
-        if (size > 0) {
-          usart_printf("\n");
-        }
-        usart_printf("%08lx: ", size);
-      }
-
-      usart_printf("%02x ", byte);
-
-      size++;
-    }
-
-    usart_printf("\n");
-
-    /* incase of error, print the position */
-    if (size != s_sdfile.ui_file_size) {
       usart_init(MYUBRR);
-
-      usart_printf_P(PSTR("ERROR reading at position: %lu\n"), size);
+      usart_printf("Could not open file");
       goto end;
+    }
+
+    /* read file byte by byte and write to portd, the delay of 17us
+       was manually calculated by timing the duration of audio
+       playback */
+    while ((byte = fat32_file_read_byte_spi(&s_sdfile)) >= 0) {
+      PORTD = byte & 0xFF;
+      _delay_us(17);
     }
   }
 
+  /* once finished, flash led */
+  while(1) {
+    writePin(PIN_ERROR, true);
+    _delay_ms(250);
+    writePin(PIN_ERROR, false);
+    _delay_ms(250);
+  }
+
  end:
-  usart_printf_P(PSTR("Done"));
+  /* on error set led to always on */
+  writePin(PIN_ERROR, true);
   while(1) {}
 }
